4f5b82ee1f953f65b964669c728a072c0681fbf1
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-06-17 15:40:05 +0100

    orchestration

--- orchestration/dags/app.py
+++ orchestration/dags/app.py
@@ -3,11 +3,9 @@ import time
 import awswrangler as wr
 import boto3
 import gspread
+from gspread.exceptions import APIError
 import pandas as pd
 from airflow.models import Variable
-
-from utils import (first_letters_to_cap, remove_trailing_space,
-                   replace_with_underscore)
 
 temp_storage_path = '/opt/airflow/tmp/'
 
@@ -19,12 +17,18 @@ def gspread_conn():
     """
 
     #filename = f"{gspread_auth_path}gspread-api-462623-c3c3b4126b29.json"
-    client = gspread.service_account(filename=Variable.get("CREDENTIALS")) 
+    client = gspread.service_account(filename=Variable.get("CREDENTIALS", deserialize_json=True)) 
     # client = gspread.service_account(filename=filename)
-    spreadsheet = client.open("marketing_source")
-    worksheet = spreadsheet.sheet1
-    rows = worksheet.get_all_values()
-    biodata = pd.DataFrame(rows[1:], columns=rows[0])
+    try:
+        spreadsheet = client.open("marketing_source")
+        worksheet = spreadsheet.sheet1
+        data = worksheet.get_all_values()
+        if data:
+            biodata = pd.DataFrame(data[1:], columns=data[0])
+        else:
+            print("data not found!")
+    except APIError as e:
+        print(e)
     print("data ingested successfully!")
     return biodata
 

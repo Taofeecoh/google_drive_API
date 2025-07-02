abfc4a5bbceca9e921e3e279bc1323ba0f230901
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-07-03 00:00:47 +0100

    orchestration

--- orchestration/dags/dag.py
+++ orchestration/dags/dag.py
@@ -1,16 +1,27 @@
+import time
 from datetime import timedelta
 
+import gspread
 import pendulum
-from airflow.models import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.models import DAG, Variable
+from airflow.operators.python import PythonOperator
 from airflow.utils import timezone
 
-from utils.app import gspread_conn, to_s3, transform_col_names
 import airflow_variable
+from utils.app import googlesheet_to_s3
 
 now = timezone.utcnow()
 a_date = timezone.datetime(2025, 6, 8)
 airflow_vars = airflow_variable
+
+client = gspread.service_account_from_dict(
+        Variable.get("CREDENTIALS_AIRFLOW_GSERVICE", deserialize_json=True)
+    )
+spreadsheet_source = "marketing_source"
+timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+bucket_key = "s3://tao-general-ingestion/xcom-googlesheet-dump/"
+file_path = "{}marketing-googlesheet-data-{}.csv".format(bucket_key, timestamp)
+
 
 args = {
     'owner': 'Taofeecoh',
@@ -30,17 +41,14 @@ with DAG(
     connect_to_driveAPI = PythonOperator(
         task_id='connect_to_driveAPI',
         provide_context=True,
-        python_callable=gspread_conn
+        python_callable=googlesheet_to_s3,
+        op_kwargs={
+            "client": client,
+            "spreadsheet_name": spreadsheet_source,
+            "file_path": file_path
+        }
+
     )
 
-    transform_data = PythonOperator(
-        task_id='transform_data',
-        python_callable=transform_col_names
-    )
 
-    load_to_s3 = PythonOperator(
-        task_id="load_to_s3",
-        python_callable=to_s3,
-    )
-
-connect_to_driveAPI >> transform_data >> load_to_s3
+connect_to_driveAPI
